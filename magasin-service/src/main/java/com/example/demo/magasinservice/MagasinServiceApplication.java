029f7d42abfbebdf7d0680d12a4ca2d680d5a17e
François GRUCHALA <@FGruchala>
2017-09-25 20:40:54 +0200

    FEAT(Magasin Service) Creation

---
+++ magasin-service/src/main/java/com/example/demo/magasinservice/MagasinServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.magasinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagasinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagasinServiceApplication.class, args);
+	}
+}
